f9224c66da9fba98b8eb52e2c5ff0058ab22f66a
eliseb318 <bachetelise42490@gmail.com>
2025-01-22 17:42:40 +0100

    Change_en_tete

--- FileHandler/FileHadler.h
+++ FileHandler/FileHadler.h
@@ -1,12 +1,18 @@
 /*************************************************************************
-                           Catalogue  -  description
+                         
                              -------------------
     début                : 22/01
-    copyright            : (C) $YEAR$ par Claire Prevost, Grigory Savchenko, Andreea-Critiana Vlad, Elise Bachet
-    e-mail               :  claire.prevost@insa-lyon.fr, grigory.savchenko@insa-lyon.fr, andreea-cristiana.vlad@insa-lyon.fr, elise.bachet@insa-lyon 
+    copyright            : (C) $YEAR$ par Claire Prevost, Grisha Savchenko, Andreea-Cristiana Vlad, Elise Bachet
+    e-mail               : claire.prevost@insa-lyon.fr, grigory.savchenko@insa-lyon.fr, andreea-cristiana.vlad@insa-lyon.fr, elise.bachet@insa-lyon.fr
 *************************************************************************/
 
-//---------- Interface de la classe <FileHandler> (fichier FileHandler.h) ----------------
+//---------- Interface de la classe <FileHandler> (fichier FileHandler.h) ------------
+#if ! defined ( FileHandler_H )
+#define FileHandler_H
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
 #include <vector>
 using namespace std;
 
--- graphe/graphe.cpp
+++ graphe/graphe.cpp
@@ -1,9 +1,9 @@
 /*************************************************************************
-                           Catalogue  -  description
+                         
                              -------------------
     début                : 22/01
-    copyright            : (C) $YEAR$ par Claire Prevost, Grigory Savchenko, Andreea-Critiana Vlad, Elise Bachet
-    e-mail               :  claire.prevost@insa-lyon.fr, grigory.savchenko@insa-lyon.fr, andreea-cristiana.vlad@insa-lyon.fr, elise.bachet@insa-lyon 
+    copyright            : (C) $YEAR$ par Claire Prevost, Grisha Savchenko, Andreea-Cristiana Vlad, Elise Bachet
+    e-mail               : claire.prevost@insa-lyon.fr, grigory.savchenko@insa-lyon.fr, andreea-cristiana.vlad@insa-lyon.fr, elise.bachet@insa-lyon.fr
 *************************************************************************/
 
-//---------- Réalisation de la classe <Graphe> (fichier graphe.cpp) ----------------
+//---------- Réalisation de la classe <Graphe> (fichier FileHandler.cpp) ------------
