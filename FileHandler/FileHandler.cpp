2b8ca72658196e9a8efdfb2edb86e346161292af
eliseb318 <bachetelise42490@gmail.com>
2025-01-22 17:52:10 +0100

    change_en_tete_jpp

---
+++ FileHandler/FileHandler.cpp
@@ -0,0 +1,9 @@
+/*************************************************************************
+                         
+                             -------------------
+    début                : 22/01
+    copyright            : (C) $YEAR$ par Claire Prevost, Grisha Savchenko, Andreea-Cristiana Vlad, Elise Bachet
+    e-mail               : claire.prevost@insa-lyon.fr, grigory.savchenko@insa-lyon.fr, andreea-cristiana.vlad@insa-lyon.fr, elise.bachet@insa-lyon.fr
+*************************************************************************/
+
+//---------- Réalisation de la classe <FileHandler> (fichier FileHandler.cpp) ------------
