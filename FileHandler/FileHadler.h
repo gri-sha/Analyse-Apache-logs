86509f646c0fb66c2365745cb25a260d8a3bdae4
Claire Prevost <72880999+clairette22@users.noreply.github.com>
2025-01-22 16:37:01 +0100

    file handler

---
+++ FileHandler/FileHadler.h
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+enum = {... } // pour donner un nom plus explicit aux index (autodocument)
+class FileHandler
+{
+	public : 
+		lireChamps(); // méthode qui ne lit qu'une ligne du document 
+		lireDocument(); // méthode qui lit tout le document (fait appel à lireChamps)
+		
+	protected :
+		Vector logHistory;
+};
