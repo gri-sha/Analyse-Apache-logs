aba3c9bc2b6cc36729ed36fbf78af30aad2744ae
eliseb318 <bachetelise42490@gmail.com>
2025-01-22 17:41:22 +0100

    Change_en_tete

--- FileHandler/FileHadler.h
+++ FileHandler/FileHadler.h
@@ -1,3 +1,12 @@
+/*************************************************************************
+                           Catalogue  -  description
+                             -------------------
+    début                : 22/01
+    copyright            : (C) $YEAR$ par Claire Prevost, Grigory Savchenko, Andreea-Critiana Vlad, Elise Bachet
+    e-mail               :  claire.prevost@insa-lyon.fr, grigory.savchenko@insa-lyon.fr, andreea-cristiana.vlad@insa-lyon.fr, elise.bachet@insa-lyon 
+*************************************************************************/
+
+//---------- Interface de la classe <FileHandler> (fichier FileHandler.h) ----------------
 #include <vector>
 using namespace std;
 
---
+++ graphe/graphe.cpp
@@ -0,0 +1,9 @@
+/*************************************************************************
+                           Catalogue  -  description
+                             -------------------
+    début                : 22/01
+    copyright            : (C) $YEAR$ par Claire Prevost, Grigory Savchenko, Andreea-Critiana Vlad, Elise Bachet
+    e-mail               :  claire.prevost@insa-lyon.fr, grigory.savchenko@insa-lyon.fr, andreea-cristiana.vlad@insa-lyon.fr, elise.bachet@insa-lyon 
+*************************************************************************/
+
+//---------- Réalisation de la classe <Graphe> (fichier graphe.cpp) ----------------
