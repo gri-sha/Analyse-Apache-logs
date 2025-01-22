2563616d2639a85c398c87937df921da007ad037
Claire Prevost <72880999+clairette22@users.noreply.github.com>
2025-01-22 17:33:26 +0100

    changes on FileHadler

---
+++ FileHandler/FileHandler.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+
+# include "FileHandler.h"
+
+using namespace std;
+
+
+
+string FileHandler :: lireChamps( void ) {
+
+    
+}
+
+    
