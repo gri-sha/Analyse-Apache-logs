4875f70c270d245365de54c68ee7bc8a81561893
Claire Prevost <72880999+clairette22@users.noreply.github.com>
2025-01-22 19:34:52 +0100

    FileHandler

--- FileHandler/FileHandler.cpp
+++ FileHandler/FileHandler.cpp
@@ -7,3 +7,111 @@
 *************************************************************************/
 
 //---------- Réalisation de la classe <FileHandler> (fichier FileHandler.cpp) ------------
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+# include "FileHandler.h"
+using namespace std;
+
+
+
+
+bool FileHandler :: readLine( ifstream& fichier ) {
+
+    logStruct log;           // déclarer la structure de données log de la ligne en entrée
+    string useless;
+
+    // Parsing des champs
+    if (!getline(fichier, log.ipAddress, ' ') ||
+        !getline(fichier, log.identity, ' ') ||
+        !getline(fichier, log.user, ' '))
+        return false;
+
+    // Extraction de la date et heure entre crochets []
+    if (!getline(fichier, useless, '[') || 
+        !getline(fichier, log.dateTime, ']'))
+        return false;
+
+    // Extraction de la requête HTTP entre guillemets ""
+    if (!getline(fichier, useless, '"') ||
+        !getline(fichier, log.httpMethod, ' ') ||
+        !getline(fichier, log.resource, ' ') ||
+        !getline(fichier, log.httpVersion, '"'))
+        return false;
+
+    // Extraction des autres champs
+    if (!getline(fichier, useless, ' ') ||
+        !getline(fichier, log.httpStatusCode, ' ') ||
+        !getline(fichier, log.responseSize, ' '))
+        return false;
+
+    // Extraction du référent et de l'agent utilisateur
+    if (!getline(fichier, useless, '"') ||
+        !getline(fichier, log.referer, '"') ||
+        !getline(fichier, useless, '"') ||
+        !getline(fichier, log.userAgent, '"'))
+        return false;
+
+    logHistory.push_back(log);
+
+    
+    return true;
+
+
+}
+
+bool FileHandler :: readDocument() {
+
+    ifstream fichier(fileName, ios::in);    
+
+    if (!fichier.is_open()) {
+        cerr << "Erreur : Impossible d'ouvrir le fichier.\n";
+        return false;
+    }
+
+    logStruct line;
+    while (readLine(fichier)) {}
+    return true;
+}
+
+
+ostream & operator << (ostream & out, FileHandler & file)
+{
+    for (int i=0 ; i < file.logHistory.size() ; ++i)
+    {
+        cout << "log " << i << " : " << endl;
+        cout << "\tIP Address: " << file.logHistory[i].ipAddress << endl;
+        cout << "\tIdentity: " << file.logHistory[i].identity << endl;
+        cout << "\tUser: " << file.logHistory[i].user << endl;
+        cout << "\tDate Time: " << file.logHistory[i].dateTime << endl;
+        cout << "\tHTTP Method: " << file.logHistory[i].httpMethod << endl;
+        cout << "\tResource: " << file.logHistory[i].resource << endl;
+        cout << "\tHTTP Version: " << file.logHistory[i].httpVersion << endl;
+        cout << "\tHTTP Status Code: " << file.logHistory[i].httpStatusCode << endl;
+        cout << "\tResponse Size: " << file.logHistory[i].responseSize << endl;
+        cout << "\tReferer: " << file.logHistory[i].referer << endl;
+        cout << "\tUser Agent: " << file.logHistory[i].userAgent << endl;
+    }
+    
+    return out;
+}
+
+
+
+
+int main() 
+{
+    FileHandler myFileHandler("../exemple.log");
+    myFileHandler.readDocument();
+    cout << myFileHandler << endl;
+    return 0;
+
+}
+
+    
+
+
--- FileHandler/FileHandler.h
+++ FileHandler/FileHandler.h
@@ -7,18 +7,15 @@
 *************************************************************************/
 
 //---------- Interface de la classe <FileHandler> (fichier FileHandler.h) ------------
-#if ! defined ( FileHandler_H )
-#define FileHandler_H
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
 
-//---------------------------------------------------------------- INCLUDE
-
-//-------------------------------------------------------- Include système
 #include <string>
 #include <vector>
 using namespace std;
 
 
-struct ApacheLogEntry {
+struct logStruct {
     string ipAddress;         // Adresse IP du client
     string identity;          // Identité (souvent '-')
     string user;              // Utilisateur authentifié (souvent '-')
@@ -26,8 +23,8 @@ struct ApacheLogEntry {
     string httpMethod;        // Méthode HTTP (GET, POST, etc.)
     string resource;          // Ressource demandée (ex: /page2.html)
     string httpVersion;       // Version du protocole HTTP (ex: HTTP/1.1)
-    int httpStatusCode;            // Code de statut HTTP (ex: 200)
-    int responseSize;              // Taille de la réponse en octets
+    string httpStatusCode;            // Code de statut HTTP (ex: 200)
+    string responseSize;              // Taille de la réponse en octets
     string referer;           // Référant (URL précédente)
     string userAgent;         // Agent utilisateur (navigateur)
 };
@@ -49,16 +46,16 @@ enum LogFieldIndex {
 class FileHandler {
 public:
 
-    FileHandler ( const string& fileName ) : fileName(fileName) {}
-
-    void readDocument ( void ) ;
-    string readLog ( void ) ;
+    FileHandler ( const string& fileName ) : fileName(fileName), currentLineIndex(0) {}
+    bool readDocument ( void ) ;
+    bool readLine ( ifstream& fichier ) ;
+    friend ostream & operator << (ostream & out, FileHandler & file);
 
     // créer une surcharge de l'opérateur << ? Utile ou pas, je ne sais pas. A faire si temps 
 
 protected:
     string fileName;                 
-    vector<string> logHistory; 
+    vector<logStruct> logHistory; 
     size_t currentLineIndex = 0; 
 };
 
